1e9b1dfe96203f064530a39c762a87cc00368c3a
iv1660yg <iv1660yg@metrostate.edu>
2019-10-23 20:56:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,15 +12,23 @@ include_once("db_connect.php");
 		<div class="collapse navbar-collapse" id="navbar1">
 			<ul class="nav navbar-nav navbar-left">
 				<?php if ((isset($_SESSION['user_id']) AND (($_SESSION['userType'])=="admin" ) )) { ?>
-				<li><p class="navbar-text"><strong>Welcome!</strong> You're signed in as <strong><?php echo $_SESSION['user_name']; ?></strong></p></li>
+                
+                <li><p class="navbar-text"><strong>Welcome!</strong> You're signed in as <strong><?php echo $_SESSION['firstname']. " " .$_SESSION['lastname']; ?></strong></p></li>
+				<li><a href="manage/users/index.php">Manage Users</a></li>
+				<li><a href="profile.php">My Profile</a></li>
 				<li><a href="logout.php">Log Out</a></li>
+
 				<?php } elseif ((isset($_SESSION['user_id']) AND (($_SESSION['userType'])=="enduser" ) )) { ?>
-				<li><p class="navbar-text"><strong>Welcome!</strong> You're signed in as <strong><?php echo $_SESSION['user_name']; ?></strong></p></li>
+				<li><p class="navbar-text"><strong>Welcome!</strong> You're signed in as <strong><?php echo $_SESSION['firstname']. " " .$_SESSION['lastname']; ?></strong></p></li>
 				<li><a href="logout.php">Log Out</a></li>
+
+
 				<?php } elseif (isset($_SESSION['user_id'])) { ?>
-					<li><p class="navbar-text"><strong>Welcome!</strong> You're signed in as <strong><?php echo $_SESSION['user_name']; ?></strong></p></li>
+					<li><p class="navbar-text"><strong>Welcome!</strong> You're signed in as <strong><?php echo $_SESSION['firstname']. " " .$_SESSION['lastname']; ?></strong></p></li>
 				<li><a href="logout.php">Log Out</a></li>
 				<?php } else { ?>
+
+
 				<li><a href="login.php">Login</a></li>
 				<li><a href="register.php">Sign Up</a></li>
 				<div>
