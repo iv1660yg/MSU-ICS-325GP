505fd644a96c437fdb3108b501ac58428996bb7f
iv1660yg <iv1660yg@metrostate.edu>
2019-11-03 17:29:29 -0600

    Update changepassword.php

--- changepassword.php
+++ changepassword.php
@@ -14,7 +14,7 @@ if (isset($_POST['updatepassword'])) {
 
 		if ($password1 == $password2) {
 			include_once("db_connect.php");
-			if (mysqli_query($conn, "UPDATE users SET password = " . $password1. " WHERE user_id = " .$_SESSION['user_id']." ")){
+			if (mysqli_query($conn, "UPDATE users SET password = " . md5($password1). " WHERE user_id = " .$_SESSION['user_id']." ")){
 			$error_message = "Password successfully changed!";
 			}
 		}
