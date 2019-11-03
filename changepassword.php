14c1ba924d1dc87fe98d8964ddaae8567b66c0df
iv1660yg <iv1660yg@metrostate.edu>
2019-11-03 14:34:13 -0600

    Update changepassword.php

--- changepassword.php
+++ changepassword.php
@@ -18,9 +18,34 @@ if (isset($_POST['login'])) {
 		$_SESSION['email'] = $row['email'];
 		$_SESSION['userType'] = $row['userType'];	
 		header("Location: index.php");
+
+	} elseif (isset($_POST['updatepassword'])) {
+		
+		$password1 = $_POST['password1'] ;
+		$password2 = $_POST['password2'] ;
+
+		if ($password1 == $password2) {
+			include_once("db_connect.php");
+			mysqli_query($conn, "UPDATE 'users' SET 'password' = " .$password1. " WHERE 'users'.'user_id' = " .$_SESSION['user_id']." ");
+		}
+		else {
+			$error_message = "password does not match"
+
+		}	
+
+
+
+
+
+
+
+
+
 	} else {
-		$error_message = "Incorrect Email or Password!!!";
+		$error_message = "error !!!";
 	}
+
+
 }
 ?>
 
@@ -32,9 +57,10 @@ if (isset($_POST['login'])) {
   	<fieldset>
 	<legend>Employee Login</legend>	
     <form class="login-form" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="loginform">
-      <input type="password" name="password" placeholder="password" required class="form-control" />
-      <input type="password" name="password" placeholder="password" required class="form-control" />
-      <button type="submit" name="update">Change Password</button>
+      <input type="password" name="password1" placeholder="password" required class="form-control" />
+      <input type="password" name="password2" placeholder="password" required class="form-control" />
+      <button type="submit" name="updatepassword">Change Password</button>
+	  <?php echo $error_message ?>
 	  </fieldset>
     </form>
   </div>
