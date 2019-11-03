4f15f49e331dc1f33b966e285d1a8bfa55e3c3f9
iv1660yg <iv1660yg@metrostate.edu>
2019-11-03 17:18:46 -0600

    Update changepassword.php

--- changepassword.php
+++ changepassword.php
@@ -14,7 +14,7 @@ if (isset($_POST['updatepassword'])) {
 
 		if ($password1 == $password2) {
 			include_once("db_connect.php");
-			if (mysqli_query($conn, "UPDATE 'users' SET 'password' = " .$password1. " WHERE 'users'.'user_id' = " .$_SESSION['user_id']." ")){
+			if (mysqli_query($conn, "UPDATE users SET password = " . $password1. " WHERE user_id = " .$_SESSION['user_id']." ")){
 			$error_message = "Password successfully changed!";
 			}
 		}
@@ -39,8 +39,8 @@ if (isset($_POST['updatepassword'])) {
   	<fieldset>
 	<legend>Change Password</legend>	
     <form class="login-form" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="loginform">
-      <input type="password" name="password1" placeholder="password" required class="form-control" />
-      <input type="password" name="password2" placeholder="password" required class="form-control" />
+      <input type="password" name="password1" placeholder="new password" required class="form-control" />
+      <input type="password" name="password2" placeholder="confirm password" required class="form-control" />
       <button type="submit" name="updatepassword">Change Password</button>
 	  <?php echo "<BR>" .$error_message ?>
 	  </fieldset>
