07211f23bdb1db0fad923f4a737563af3c0e2a7e
iv1660yg <iv1660yg@metrostate.edu>
2019-11-09 20:01:05 -0600

    Update index2.php

--- users/index2.php
+++ users/index2.php
@@ -11,7 +11,9 @@ if(isset($_SESSION['user_id']) =="") {
 
 <head>
   <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+
   <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>  
 </head>
 <br />
